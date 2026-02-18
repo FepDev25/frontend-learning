18d90a5d85586ed855d3ce8745335095484dbb39
Felipe <lamediatricolor@gmail.com>
2026-02-17 21:44:04 -0500

    moviendo frontend masters a carpeta correspondiente

---
+++ frontend-masters/Path00 - Computer Science/Course01 - Complete Intro to CS/code-complete-intro-cs/src/heap-sort.test.js
@@ -0,0 +1,41 @@
+/*
+  
+  Create a function called heapSort that accepts an array and performs a heap sort on it in place (heap sorts are normally destructive)
+  
+  You will probably need at least two more functions: heapify and createMaxHeap
+   
+*/
+
+const heapSort = (array) => {
+  array = createMaxHeap(array);
+  for (let i = array.length - 1; i > 0; i--) {
+    swapPlace(0, i, array);
+    heapify(array, 0, i);
+  }
+
+
+  return array;
+};
+
+const swapPlace = (index1, index2, array) => {
+  let temp = array[index1];
+  array[index1] = array[index2];
+  array[index2] = temp;
+  return array;
+};
+
+const createMaxHeap = (array) => {
+  // code
+};
+
+const heapify = (array, index, heapSize) => {
+  // code
+};
+
+// unit tests
+// do not modify the below code
+test("heap sort", function () {
+  const nums = [2, 5, 3, 8, 10, 6, 4, 7, 9, 1];
+  heapSort(nums);
+  expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+});
